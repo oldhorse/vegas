22ef3f7349fd391aa546bbfb1b23b062ef1eb313
majunlong <majunlong@rd.tuan800.com>
2014-06-27 13:48:49 +0800

    some refactor

--- Gemfile
+++ Gemfile
@@ -9,11 +9,22 @@ gem 'sqlite3'
 
 group :development, :test do
   gem 'rspec-rails', '2.13.1'
+  gem 'guard-rspec', '2.5.0'
 end
 
 group :test do
   gem 'selenium-webdriver', '2.35.1'
   gem 'capybara', '2.1.0'
+  # Uncomment this line on OS X.
+  # gem 'growl', '1.0.3'
+
+  # Uncomment these lines on Linux.
+  gem 'libnotify', '0.8.0'
+
+  # Uncomment these lines on Windows.
+  # gem 'rb-notifu', '0.0.4'
+  # gem 'win32console', '1.3.2'
+  # gem 'wdm', '0.1.0'
 end
 
 # Use SCSS for stylesheets
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+   let(:base_title) { "Vegas" }
+
   describe "Home page" do
     it "should have the content 'Vegas' " do
       visit '/static_pages/home'
@@ -10,7 +12,11 @@ describe "StaticPages" do
     end
     it "should have the title 'Home' " do
       visit '/static_pages/home'
-      expect(page).to have_title("Vegas | Home")
+      expect(page).to have_title("#{base_title}")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
@@ -21,7 +27,7 @@ describe "StaticPages" do
     end
     it "should have the title 'Help' " do
       visit '/static_pages/help'
-      expect(page).to have_title("Vegas | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -32,7 +38,7 @@ describe "StaticPages" do
     end
     it "should have the title 'About Us' " do
       visit '/static_pages/about'
-      expect(page).to have_title("Vegas | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
 
